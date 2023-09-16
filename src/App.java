2e27bd0731190b013137fb12a6ff771f870fc4e3
Mehmet Fatih Sanver <mehmet-fatih.sanver@tu-dortmund.de>
2023-09-16 11:58:45 +0200

    added audio related buttons

--- src/App.java
+++ src/App.java
@@ -7,31 +7,33 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.Timer;
+
 
 public class App extends JFrame
 {
-    private JButton startButton, resetButton, skipButton;
-    private JPanel timerPanel, statePanel;
-    Timer timer;
-    private JLabel sessionAmount, timerLabel, stateLabel;
-    private int minutes = 25;
-    private int seconds = 0;
-    protected int sessionsCompleted = 0;
-    protected int totalMinutesWorked = 0;
-    public Log_Updater lUpdater = new Log_Updater();
-    public TIMER_STATES currentState;
-    Logic logic = new Logic();
+    protected JButton startButton, resetButton, skipButton, audioStartButton, audioForwardButton, audioBackwardButton, rainButton;
+    protected JPanel timerPanel, statePanel;
+    protected JLabel sessionAmount, timerLabel, stateLabel, songName, progressBar;
+    boolean rainButtonPressed = false;
+
     
+    public Log_Updater lUpdater = new Log_Updater();
+    public Audio audio = new Audio();
+    
+    Icons icons = new Icons();
+    TIMER_STATES currentState = TIMER_STATES.IDLE;
+    TimerManager tm = new TimerManager(currentState, this);
 
     Font buttonFont = new Font("Arial", Font.BOLD, 10);
+    
 
-    public App()
+    public void makeUI() 
     {
+        
+
         // The Frame
-        final int WIDTH = 300;
-        final int HEIGHT = 300;
+        final int WIDTH = 450;
+        final int HEIGHT = 450;
         this.setResizable(false);
         this.setTitle("Pomodoro Timer");
         this.setSize(WIDTH, HEIGHT);
@@ -40,16 +42,15 @@ public class App extends JFrame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         
-        // Set Initial State to IDLE 
-        currentState = TIMER_STATES.IDLE;
+       
 
         // Timer and State Panels
         timerPanel = new JPanel();
-        timerPanel.setBounds(50, 75, 200, 50);
+        timerPanel.setBounds(125, 75, 200, 50);
         timerPanel.setBackground(Color.black);
 
         statePanel = new JPanel();
-        statePanel.setBounds(50, 10, 200, 60);
+        statePanel.setBounds(125, 10, 200, 60);
         statePanel.setBackground(Color.black);
 
         // Timer Label
@@ -62,15 +63,16 @@ public class App extends JFrame
 
         // State Label
         stateLabel = new JLabel("SESSION");
-        stateLabel.setBounds(50, 10, 200, 60);
+        stateLabel.setBounds(125, 10, 200, 60);
         stateLabel.setBackground(Color.black);
         stateLabel.setForeground(Color.white);
         stateLabel.setFont(new Font("Arial", Font.BOLD, 24));
         statePanel.add(stateLabel);
 
         // Amount of pomodoros done
-        sessionAmount = new JLabel("Pomodoros: " + sessionsCompleted);
-        sessionAmount.setBounds(90, 220, 120, 30);
+        sessionAmount = new JLabel("Pomodoros: " + tm.sessionsCompleted);
+        sessionAmount.setBounds(165, 200, 120, 30);
+        sessionAmount.setHorizontalAlignment(JLabel.HORIZONTAL);
         sessionAmount.setBackground(Color.black);
         sessionAmount.setForeground(Color.white);
         sessionAmount.setFont(new Font("Arial", Font.BOLD, 13));
@@ -79,58 +81,164 @@ public class App extends JFrame
 
         // Start Button
         startButton = new JButton("Start");
-        startButton.setBounds(45, 150, 70, 30);
+        startButton.setBounds(120, 150, 70, 30);
         startButton.setFocusPainted(false);
         startButton.setBackground(Color.black);
         startButton.setForeground(Color.white);
         startButton.setFont(buttonFont);
+        startButton.addActionListener(new ActionListener() {  // TODO I have no fucking idea how to make this work... Maybe ask Utku or Leon?
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (currentState == TIMER_STATES.IDLE) {
+                    tm.start();
+                    tm.checkButtonEnability();
+                } else if (currentState == TIMER_STATES.SESSION) {
+                    tm.stop();
+                    tm.checkButtonEnability();
+                } else {
+                    tm.checkButtonEnability();
+                }
+                toggleTimer();
+                tm.updateTimer();
+            }
+        });
         
         
-       
         
-
+        
+        // Reset Button 
         resetButton = new JButton("Reset");
-        resetButton.setBounds(115, 150, 70, 30);
+        resetButton.setBounds(190, 150, 70, 30);
         resetButton.setFocusPainted(false);
         resetButton.setBackground(Color.black);
         resetButton.setForeground(Color.white);
         resetButton.setFont(buttonFont);
-        
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed( ActionEvent e )
+            {
+                if( currentState == TIMER_STATES.IDLE )
+                { tm.resetTimer(); }
 
+                else if( currentState == TIMER_STATES.SESSION )
+                { tm.resetTimer(); }
+
+                else 
+                { resetButton.setEnabled(false); }
+            }
+        });
+        
         // Skip Button (only for break times)
         skipButton =  new JButton("Skip");
-        skipButton.setBounds(185, 150, 70, 30);
+        skipButton.setBounds(260, 150, 70, 30);
         skipButton.setFocusPainted(false);
         skipButton.setBackground(Color.black);
         skipButton.setForeground(Color.white);
         skipButton.setFont(buttonFont);
         
-       
+        //Audio Backwards Button (This button restarts the audio from the beginning)
+        audioBackwardButton = new JButton(icons.setIcon("src/resources/restart.png", 70, 40));
+        audioBackwardButton.setBounds(105, 260, 70, 40);
+        audioBackwardButton.setFocusPainted(false);
+        audioBackwardButton.setBackground(Color.black);
+        audioBackwardButton.setForeground(Color.white);
+        audioBackwardButton.setFont(buttonFont);
+        
+        //Audio Forward Button (This button starts the next song )
+        // TODO draw the button icons
+        audioForwardButton = new JButton(icons.setIcon("src/resources/next_song.png", 70, 40));
+        audioForwardButton.setBounds(275, 260, 70, 40);
+        audioForwardButton.setFocusPainted(false);
+        audioForwardButton.setBackground(Color.black);
+        audioForwardButton.setForeground(Color.white);
+        audioForwardButton.setFont(buttonFont);
+        
+        // Audio Start Button
+        audioStartButton = new JButton(icons.setIcon("src/resources/start.png", 50, 40));
+        audioStartButton.setBounds(175, 260, 50, 40);
+        audioStartButton.setFocusPainted(false);
+        audioStartButton.setBackground(Color.black);
+        audioStartButton.setForeground(Color.white);
+        
+        // Rain Button
+        rainButton = new JButton(icons.setIcon("src/resources/rain.png", 50, 40));
+        rainButton.setBounds(225, 260, 50, 40);
+        rainButton.setBackground(Color.black);
+        rainButton.setForeground(Color.white);
+        rainButton.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              if( !rainButtonPressed ) // TODO draw the stop rain icon
+              {
+                   rainButtonPressed = true;  
+              }
+              else
+              {
+                rainButtonPressed = false;
+              }
+              audio.ringTone(1);
+            }
+            
+        });
+
+        // Song Name Label
+        songName = new JLabel(audio.song_Name);
+        songName.setBounds(150,300,150,40);
+        songName.setHorizontalAlignment(JLabel.HORIZONTAL);
+        songName.setBackground(Color.black);
+        songName.setForeground(Color.white);
+        songName.setFont(new Font("Arial", Font.BOLD, 12));
+
+        // Progress Bar 
+        progressBar = new JLabel("");
+        progressBar.setBounds(100,350, 250, 20);
+        progressBar.setBackground(Color.black);
+        progressBar.setForeground(Color.white);
+        progressBar.setFont(new Font("Purisa", Font.BOLD, 15));
+        progressBar.setHorizontalAlignment(JLabel.HORIZONTAL);
         
 
-
+       
+        
         this.add(startButton);
         this.add(resetButton);
         this.add(skipButton);
         this.add(timerPanel);
         this.add(statePanel);
+        this.add(audioBackwardButton);
+        this.add(audioForwardButton);
+        this.add(audioStartButton);
+        this.add(songName);
+        this.add(progressBar);
+        this.add(rainButton);
         this.setVisible(true);
         
     }
 
-    
+    public void toggleTimer()
+    {
+        if( currentState == TIMER_STATES.SESSION )
+        {
+            startButton.setText("Pause");
+            
+        }
+        else if( currentState == TIMER_STATES.PAUSED )
+        {
+            
+            startButton.setText("Resume");
+            
+           
+        }
+        else if( currentState == TIMER_STATES.IDLE )
+        {
+            startButton.setText("Start");
+            
+        }
+    }
     
    
-    
+     
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new App();
-            }
-        });
-        }
 
 }
