16ec41824ed747475e45db54c092ad32ad72ed3f
Mehmet Fatih Sanver <mehmet-fatih.sanver@tu-dortmund.de>
2023-09-16 14:04:35 +0200

    updated Notifications.java

--- src/Notifications.java
+++ src/Notifications.java
@@ -1,8 +1,31 @@
 package src;
+
+import java.awt.*;
+import java.awt.TrayIcon.MessageType;
+
+
+
 public class Notifications 
 {
-    protected void notify( TIMER_STATES states )
-    {
-
+    protected void showNotification(String title, String message, MessageType messageType) {
+        
+        if (SystemTray.isSupported()) {
+            SystemTray tray = SystemTray.getSystemTray();
+            
+            Image image = Toolkit.getDefaultToolkit().getImage("/src/resources/app_icon"); // Replace "icon.png" with your app's icon file path
+    
+            TrayIcon trayIcon = new TrayIcon(image, "Pomodoro Timer");
+            trayIcon.setImageAutoSize(true);
+    
+            try {
+                tray.add(trayIcon);
+                trayIcon.displayMessage(title, message, messageType);
+                
+            } catch (AWTException e) {
+                System.err.println("SystemTray could not be added.");
+                e.printStackTrace();
+            }
+        }
     }
+    
 }
