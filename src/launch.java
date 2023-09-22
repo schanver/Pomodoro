422a8130d03a85efc00cf6294a0a1351a931e4f2
Mehmet Fatih Sanver <mehmet-fatih.sanver@tu-dortmund.de>
2023-09-23 00:31:31 +0200

    tested some stuff

--- src/launch.java
+++ src/launch.java
@@ -1,23 +1,45 @@
 package src;
 
 
-import java.awt.TrayIcon.MessageType;
+
+
+
+
+
+
 
 import javax.swing.SwingUtilities;
 
-public class launch {
 
+public class launch {
+    
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        /*SwingUtilities.invokeLater(new Runnable() {
+            App app = new App();
             @Override
             public void run() {
-                /*App app = new App();
-                app.makeUI();*/
-                Notifications notifications = new Notifications();
-                notifications.showNotification("End of the Session", "Your session is over." + '\n'+ "Enjoy your hard earned break!", MessageType.INFO);
-         
+               
+                app.makeUI();
+                
+              // Notifications notifications = new Notifications();
+                //notifications.showNotification("Pomodoro", "Let's start the session", MessageType.INFO);
+                
             }
         });
-        }
+         Runtime.getRuntime().addShutdownHook(new Thread() {
+            App app = new App();
+                @Override
+                public void run() {
+                    app.handleShutdown();
+                }
     
+        });
+    
+    }*/
+    App app = new App();
+    app.currentState = TIMER_STATES.SESSION;
+    Notifications notifications = new Notifications();
+    notifications.showNotification(app);
+
+}
 }
