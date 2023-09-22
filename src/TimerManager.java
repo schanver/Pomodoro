13ce51cd34920f7d8fe17b635f11ced08c5c3da1
Mehmet Fatih Sanver <mehmet-fatih.sanver@tu-dortmund.de>
2023-09-23 00:32:36 +0200

    updated TimerManager.java

--- src/TimerManager.java
+++ src/TimerManager.java
@@ -6,10 +6,11 @@ import javax.swing.Timer;
 
 public class TimerManager  {
     protected Timer timer;
-    protected int minutes = 25;
+    protected int minutes = 1;  //TODO: Change this to 25 after testing
     protected int seconds = 0;
     protected int sessionsCompleted = 0;
     protected int totalMinutesWorked = 0;
+    protected int totalSecondsWorked = 0;
    
     Log_Updater lUpdater = new Log_Updater();
     Notifications notifications = new Notifications();
@@ -39,13 +40,14 @@ public class TimerManager  {
     
     
    
-    public void updateTimer() {  //MAKE A FCKING LOGIC CHART FOR THIS FUNCTION DUMBASS, DON'T TRY TO BRUTE-FORCE IT
+    public void updateTimer() {  /* F: MAKE A FCKING LOGIC CHART FOR THIS FUNCTION DUMBASS, DON'T TRY TO BRUTE-FORCE IT
+                                M: It works now though, my intelligence needs no logic chart! */ 
         if (minutes == 0 && seconds == 0) {
             if (app.currentState == TIMER_STATES.SESSION) {
                 sessionsCompleted++;
                 lUpdater.logSessionData(sessionsCompleted, totalMinutesWorked);
-                audio.ringTone(0);
-                notifications.showNotification("End of the Session", "Your session is over. Enjoy your hard earned break!", null);
+                //audio.ringTone(0);
+                //notifications.showNotification("End of the Session", "Your session is over. Enjoy your hard earned break!", null);
                 }
                 switchStates();
     
@@ -59,6 +61,7 @@ public class TimerManager  {
             }
         }
         updateTimerLabel();
+        app.sessionAmount.setText("Pomodoros:" + sessionsCompleted);
     }
     
 
@@ -75,12 +78,13 @@ public class TimerManager  {
             minutes = 25;
             seconds = 0;
             app.currentState = TIMER_STATES.IDLE;
-            updateTimer();
+            stop();
+            updateTimerLabel();
             app.startButton.setText("Start");
         }
         
     }
-    public void checkButtonEnability()
+    public void checkButtonEnability()  //  This function checks the availability of the buttons and disables them depending on the state.
     {
         if( app.currentState == TIMER_STATES.SESSION || app.currentState == TIMER_STATES.PAUSED || app.currentState == TIMER_STATES.IDLE )
         {
@@ -103,22 +107,26 @@ public class TimerManager  {
         if (app.currentState == TIMER_STATES.SESSION) {
             if (sessionsCompleted > 0 && sessionsCompleted % 4 == 0) {
                 app.currentState = TIMER_STATES.LONG_BREAK;
+                app.stateLabel.setText("LONG BREAK");
                 minutes = 15;
                 seconds = 0;
                 
             } else {
                 app.currentState = TIMER_STATES.SHORT_BREAK;
+                app.stateLabel.setText("SHORT BREAK");
                 minutes = 5;
                 seconds = 0;
             }
         } else if (app.currentState == TIMER_STATES.SHORT_BREAK || app.currentState == TIMER_STATES.LONG_BREAK) {
             app.currentState = TIMER_STATES.SESSION;
-            minutes = 25;
+            app.stateLabel.setText("SESSION");
+            minutes = 1; //TODO change this after the tests
             seconds = 0;
         }
     
         // Call updateTimer only once after updating minutes and seconds
         updateTimer();
+        checkButtonEnability();
         
     }
     
