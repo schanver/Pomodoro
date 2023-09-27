d19a3bee1cf76e70a1fefd53c220a1b008a0e5c5
Mehmet Fatih Sanver <mehmet-fatih.sanver@tu-dortmund.de>
2023-09-27 22:43:16 +0200

    minor changes

--- src/launch.java
+++ src/launch.java
@@ -1,45 +1,35 @@
 package src;
 
-
-
-
-
-
-
-
-
 import javax.swing.SwingUtilities;
 
 
 public class launch {
     
     public static void main(String[] args) {
-        /*SwingUtilities.invokeLater(new Runnable() {
-            App app = new App();
+        
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run() {
-               
-                app.makeUI();
+            public void run() 
+            {   
+                App app = new App();
+                TimerManager tm = new TimerManager(TIMER_STATES.IDLE, app);
+                TimerEditor.makeUI(app, tm);
                 
-              // Notifications notifications = new Notifications();
-                //notifications.showNotification("Pomodoro", "Let's start the session", MessageType.INFO);
                 
             }
         });
          Runtime.getRuntime().addShutdownHook(new Thread() {
-            App app = new App();
+            
                 @Override
                 public void run() {
+                    App app = new App();
                     app.handleShutdown();
                 }
     
         });
+        
+    }
     
-    }*/
-    App app = new App();
-    app.currentState = TIMER_STATES.SESSION;
-    Notifications notifications = new Notifications();
-    notifications.showNotification(app);
 
 }
-}
+
